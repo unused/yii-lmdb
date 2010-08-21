c414f69271bf7373f075acdd4b06bc03447e4234
Christian Mayer <github@mifix.com>
2010-08-21 18:44:57 +0800

    Added initial admin user sql file

---
+++ protected/data/add-admin-user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`id`,`username`,`password`,`salt`,`avatar`,`last_login_at`,`online_at`,`created_at`,`updated_at`)
+VALUES
+(1,'admin'admin,'7d2ac9e8b81df220bf6c83a85aeb72aa93dd6935','d97b1134a43d0d169fd58783d7aa1c2e','','2010-08-21 10:38:59',NULL,'2010-08-21 10:38:39','2010-08-21 10:38:59');
